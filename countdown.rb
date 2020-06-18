29bf91135113d64a58d49cd920975097b1d2c217
jasonpatino94 <jason.patino94@gmail.com>
2020-06-18 03:54:45 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -4,5 +4,10 @@ def countdown(num)
     while num > 0
         puts "#{num} SECOND(S)!"
         num -= 1
-        exit
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+    sleep(x)
 end
