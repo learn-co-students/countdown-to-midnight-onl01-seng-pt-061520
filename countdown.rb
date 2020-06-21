9a15792061e49e0282b9c17aa5380a5c09d695c3
MUHANNAD JABER <abujaber44@gmail.com>
2020-06-21 22:27:36 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown_with_sleep(seconds_to_midnight)
+  sleep(5)
 end
+
+
+
+
+
+def countdown(number)
+  
+  x = 10 
+while x > 0 
+  puts "#{x} SECOND(S)!"
+  x -= 1
+end
+return "HAPPY NEW YEAR!"
+
+end 
+
+ 
