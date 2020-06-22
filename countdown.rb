79643dc8f0ab82bc9aec03d2d7e6ef5ec58c4697
Galilea Bernal <galilea.bernal98@gmail.com>
2020-06-22 18:03:49 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
+seconds_to_midnight = 10
 
-def countdown
+def countdown(seconds_to_midnight)
+ while seconds_to_midnight > 0
+   puts "#{seconds_to_midnight} SECOND(S)!"
+   seconds_to_midnight -= 1
+  end
+  if seconds_to_midnight == 0
+    return  "HAPPY NEW YEAR!"
+  end
+end
+
+def countdown_with_sleep(seconds_to_midnight)
+  sleep(5)
 end
