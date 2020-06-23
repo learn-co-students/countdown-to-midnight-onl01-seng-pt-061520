c36a15ff2eca86f755b685c356351250d3ba4ba8
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-23 02:23:37 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,29 @@
-#write your code here
+#require'pry'
 
-def countdown
+seconds_to_midnight = 10
+
+ def countdown_with_sleep(seconds_to_midnight)
+   
+   while seconds_to_midnight > 0 
+   puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1 
+      sleep(5)
+   
+  end 
+  
+    return   "HAPPY NEW YEAR!"
+
+end 
+
+
+def countdown(seconds_to_midnight)
+  
+ while seconds_to_midnight > 0 
+   puts "#{seconds_to_midnight} SECOND(S)!"
+   seconds_to_midnight -= 1 
+   
+  end 
+    return  "HAPPY NEW YEAR!"
+    
 end
+
